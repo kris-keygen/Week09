54c84d2883902dff015f709d102ee15c4e4aae70
kris-keygen <kristiankeaganl@gmail.com>
2025-11-05 11:08:30 +0700

    Interface Animal

--- src/week09/kristian/umn/ac/id/Animal.java
+++ src/week09/kristian/umn/ac/id/Animal.java
@@ -1,5 +1,25 @@
 package week09.kristian.umn.ac.id;
 
-public class Animal {
+public interface Animal {
+	public void eat();
+	public void travel();
+}
 
+public class Mammal implements Animal{
+	public Mammal() {
+		
+	}
+	public void eat() {
+		System.out.println("Mammal eats");
+	}
+	
+	public void travel() {
+		System.out.println("Mammal travel");
+	}
+	
+	public static void main(String[]args) {
+		Mammal m = new Mammal();
+		m.eat();
+		m.travel();
+	}
 }
